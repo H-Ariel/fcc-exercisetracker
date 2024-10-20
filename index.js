707e9aff2be3ff8d85e1ae2fdd4bdf804fa2d2e7
ariel halili <arielhalili259@gmail.com>
2024-10-20 15:41:50 +0300

    update index.js

--- index.js
+++ index.js
@@ -3,14 +3,83 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  }
+});
+const exerciseSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  description: String,
+  duration: Number,
+  date: String
+});
+const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
 
+app.post('/api/users/', (req, res) => {
+  const username = { username: req.body.username };
+  new User(username).save()
+    .then(() => {
+      User.findOne(username)
+        .then(data => res.json(data))
+        .catch(err => res.json(err));
+    })
+    .catch(err => res.json(err));
+});
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  User.findOne({ _id: req.params._id })
+    .then(data => {
+      const exec = {
+        _id: req.params._id,
+        username: data.username,
+        description: req.body.description,
+        duration: parseInt(req.body.duration),
+        date: new Date(req.body.date).toDateString()
+      };
+      new Exercise(exec).save()
+        .then(() => res.json(exec))
+        .catch(err => res.json(err));
+
+    })
+    .catch(err => res.json(err));
+});
+
+app.get('/api/users/:_id/logs?[from][&to][&limit]', (req, res) => {
+  const { from, to, limit } = req.query;
+  const _id = req.params._id;
+  let query = { _id };
+
+  if (from || to) {
+    query.date = {};
+    if (from) query.date.$gte = new Date(from);
+    if (to) query.date.$lte = new Date(to);
+  }
+
+  Exercise.find(query)
+    .sort({ date: 1 })
+    .limit(parseInt(limit) || 0)
+    .then(dataList => res.json(dataList))
+    .catch(err => res.json(err));
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
